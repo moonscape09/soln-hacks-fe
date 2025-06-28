0f9a009f9fc3be90f1250f325b5bac15a867e2ad
moonscape09 <munim.adil@outlook.com>
2025-06-28 16:27:39 -0400

    Fix json headers

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -4,9 +4,9 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
 export const createObjectFromPrompt = async (prompt) => {
     try {
-        const response = await axios.post(`${API_BASE_URL}/api/interpret`, {"prompt": prompt}, headers: {
+        const response = await axios.post(`${API_BASE_URL}/api/interpret`, { "prompt": prompt }, { headers: {
             'Content-Type': 'application/json'
-        })
+    }})
         return response.data;
     } catch (error) {
         console.error("Error creating object: ", error);
